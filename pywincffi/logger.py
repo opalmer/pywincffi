7a345cec7a749cb271d018cef522b6965b3e029a
Oliver Palmer <oliverpalmer@opalmer.com>
2015-06-15 22:01:21 -0400

    adding logger.py and tests

---
+++ pywincffi/logger.py
@@ -0,0 +1,46 @@
+"""
+Logger
+======
+
+This module contains pywincffi's logger, formatter and other related
+utilities.
+"""
+
+import logging
+
+try:
+    NullHandler = logging.NullHandler
+
+except AttributeError:  # Python 2.6
+    class NullHandler(logging.Handler):
+        def handle(self, record):
+            pass
+
+        def emit(self, record):
+            pass
+
+        def createLock(self):
+            self.lock = None
+
+
+logger = logging.getLogger("pywincffi")
+logger.addHandler(NullHandler())
+
+def configure(level, handler=None, formatter=None):
+    """
+    Enables pywincffi's logger, add formatting, handling and set
+    the logging level.  Normally pywincffi's logger is not configured
+    except for an
+    """
+    if formatter is None:
+        formatter = logging.Formatter(
+            "%(asctime)s %(name)s %(levelname)9s %(message)s",
+            datefmt="%D %H:%M:%S"
+        )
+
+    if handler is None:
+        handler = logging.StreamHandler()
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(level)
---
+++ tests/test_logger.py
@@ -0,0 +1,55 @@
+import logging
+import os
+from unittest import TestCase
+
+from pywincffi.logger import NullHandler, logger, configure
+
+class TestLogger(TestCase):
+    def setUp(self):
+        self.handlers = logger.handlers[:]
+        self.level = logger.level
+
+    def tearDown(self):
+        logger.handlers[:] = self.handlers
+        logger.setLevel(self.level)
+
+    def test_default_handlers(self):
+        self.assertEqual(
+            [type(handler) for handler in logger.handlers],
+            [type(NullHandler())]
+        )
+
+    def test_configure_level(self):
+        configure(logging.CRITICAL)
+        self.assertEqual(logger.level, logging.CRITICAL)
+
+    def test_configure_default_handler(self):
+        configure(logging.CRITICAL)
+        self.assertEqual(
+            [type(handler) for handler in logger.handlers],
+            [type(NullHandler()), type(logging.StreamHandler())]
+        )
+
+    def test_configure_default_formatter(self):
+        configure(logging.CRITICAL)
+        handler = logger.handlers[-1]
+        self.assertEqual(
+            handler.formatter._fmt,
+            "%(asctime)s %(name)s %(levelname)9s %(message)s"
+        )
+        self.assertEqual(
+            handler.formatter.datefmt, "%D %H:%M:%S"
+        )
+
+    def test_configure_custom_handler(self):
+        handler = logging.FileHandler(os.devnull)
+        self.addCleanup(handler.close)
+        configure(logging.CRITICAL, handler)
+        self.assertIs(logger.handlers[-1], handler)
+
+    def test_configure_custom_formatter(self):
+        formatter = logging.Formatter(fmt="1234", datefmt="4567")
+        configure(logging.CRITICAL, formatter=formatter)
+        handler = logger.handlers[-1]
+        self.assertEqual(handler.formatter._fmt, "1234")
+        self.assertEqual(handler.formatter.datefmt, "4567")
