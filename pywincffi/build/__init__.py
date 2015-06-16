ca420fab014ed1cc627f9cf8c3692a3959794bfc
Oliver Palmer <oliverpalmer@opalmer.com>
2015-06-15 21:50:36 -0400

    minor wording fix

--- pywincffi/build/__init__.py
+++ pywincffi/build/__init__.py
@@ -6,7 +6,7 @@ This :mod:`pywincffi.build` package is used by pywincffi's build system.  It's
 primary purpose is to consume events from GitHub, run builds and report back
 to GitHub with results.
 
-This Python package is not included as part of the distribution to PyPi.  The
+This Python package is not included as part of the distribution on PyPi.  The
 package must be manually installed by setting ``PYWINCFFI_INSTALL_BUILD`` in
 the environment prior to running the setup.py script.
 """
