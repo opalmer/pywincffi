7200e1415ef514ef0229d174e7d1161bd3787224
Oliver Palmer <oliverpalmer@opalmer.com>
2015-06-28 23:18:08 -0400

    adding initial bind() test for testing

--- tests/test_core/test_library.py
+++ tests/test_core/test_library.py
@@ -1,4 +1,9 @@
+import os
+import tempfile
+from textwrap import dedent
+
 from cffi import FFI
+
 from pywincffi.core import library
 from pywincffi.testutil import TestCase
 
@@ -12,3 +17,21 @@ class TestFFI(TestCase):
 
     def test_instance(self):
         self.assertIsInstance(library.ffi, FFI)
+
+
+class TestBind(TestCase):
+    def test_bind(self):
+        ffi = FFI()
+        ffi.set_unicode(True)
+
+        fd, path = tempfile.mkstemp()
+        self.addCleanup(self.remove, path)
+
+        with os.fdopen(fd, "wb") as temp_stream:
+            data = dedent("""
+            #define PROCESS_QUERY_INFORMATION 0x0400
+            """)
+            temp_stream.write(data.encode())
+
+        kernel32 = library.bind("kernel32", path, ffi_=ffi)
+        print(dir(kernel32))
