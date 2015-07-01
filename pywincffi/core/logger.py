02a9dda787638bb85f47f92b101a82d1be1ff06a
Oliver Palmer <oliverpalmer@opalmer.com>
2015-06-30 21:49:44 -0400

    fixing doc string header

---
+++ pywincffi/core/logger.py
@@ -0,0 +1,48 @@
+"""
+Logger
+------
+
+This module contains pywincffi's logger and provides functions to
+configure the logger at runtime.
+"""
+
+import logging
+
+try:
+    NullHandler = logging.NullHandler
+
+except AttributeError:  # pragma: no cover
+    # Python 2.6 does not have a NullHandler
+    class NullHandler(logging.Handler):
+        def handle(self, record):
+            pass
+
+        def emit(self, record):
+            pass
+
+        def createLock(self):
+            self.lock = None
+
+
+logger = logging.getLogger("pywincffi")
+logger.addHandler(NullHandler())
+
+
+def configure(level, handler=None, formatter=None):
+    """
+    Enables pywincffi's logger, add formatting, handling and set
+    the logging level.  Normally pywincffi's logger is not configured
+    except for an
+    """
+    if formatter is None:
+        formatter = logging.Formatter(
+            "%(asctime)s %(name)s %(levelname)9s %(message)s",
+            datefmt="%D %H:%M:%S"
+        )
+
+    if handler is None:
+        handler = logging.StreamHandler()
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(level)
