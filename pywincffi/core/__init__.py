da6935f820339ff304b09af53ae441eef21b14a8
Oliver Palmer <oliverpalmer@opalmer.com>
2015-07-04 19:45:39 -0400

    enhance doc for pywincffi.core

--- pywincffi/core/__init__.py
+++ pywincffi/core/__init__.py
@@ -2,5 +2,7 @@
 Core
 ====
 
-The core package used internally by pywincffi.
+The core package used internally by pywincffi.  This package contains
+wrappers for :class:`cffi.api.FFI`, a logger and some basic code used
+by the unittests.
 """
