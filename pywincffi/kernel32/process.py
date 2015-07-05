ceadb5dc07aa811abe93d635f52cc6c2617fbd07
Oliver Palmer <oliverpalmer@opalmer.com>
2015-07-05 09:05:33 -0400

    creating kernel32 package

---
+++ pywincffi/kernel32/process.py
@@ -0,0 +1,83 @@
+"""
+Process
+-------
+
+Provides functions, constants and utilities that wrap the Windows
+functions associated with process management and interaction.  This
+module also provides several constants as well, see Microsoft's
+documentation for the constant names and their purpose:
+
+    * **Process Security and Access Rights** -
+      https://msdn.microsoft.com/en-us/library/windows/desktop/ms684880
+
+.. note::
+
+    Not all constants may be defined
+"""
+
+import six
+from pywincffi.core.ffi import bind, ffi, input_check, error_check
+
+_kernel32 = bind(ffi, "kernel32")
+
+PROCESS_CREATE_PROCESS = 0x0080
+PROCESS_CREATE_THREAD = 0x0002
+PROCESS_DUP_HANDLE = 0x0040
+PROCESS_QUERY_INFORMATION = 0x0400
+PROCESS_QUERY_LIMITED_INFORMATION = 0x1000
+PROCESS_SET_INFORMATION = 0x0200
+PROCESS_SET_QUOTA = 0x0100
+PROCESS_SUSPEND_RESUME = 0x0800
+PROCESS_TERMINATE = 0x0001
+PROCESS_VM_OPERATION = 0x0008
+PROCESS_VM_READ = 0x0008
+PROCESS_VM_WRITE = 0x0020
+SYNCHRONIZE = 0x00100000
+
+
+def SetLastError(dwErrCode):
+    """
+    Sets the last error code using the Windows API.  This function
+    generally should only be used by tests.
+
+    :param int dwErrCode:
+        The error code to set
+    """
+    input_check("dwErrCode", dwErrCode, six.integer_types)
+    _kernel32.SetLastError(ffi.cast("DWORD", dwErrCode))
+
+
+def OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId):
+    """
+    Opens an existing local process object.
+
+    :param int dwDesiredAccess:
+        The required access to the process object.
+
+    :param bool bInheritHandle:
+        Enables or disable handle inheritance for child processes.
+
+    :param int dwProcessId:
+        The id of the local process to be opened.
+
+    :returns:
+        Returns a handle to the opened process in the form of
+        a void pointer.  This value can be used by other functions
+        such as :func:`TerminateProcess`
+
+    .. seealso::
+
+        https://msdn.microsoft.com/en-us/library/windows/desktop/ms684320
+    """
+    input_check("dwDesiredAccess", dwDesiredAccess, six.integer_types)
+    input_check("bInheritHandle", bInheritHandle, bool)
+    input_check("dwProcessId", dwProcessId, six.integer_types)
+
+    handle_id = _kernel32.OpenProcess(
+        ffi.cast("DWORD", dwDesiredAccess),
+        ffi.cast("BOOL", bInheritHandle),
+        ffi.cast("DWORD", dwProcessId)
+    )
+    error_check("OpenProcess")
+
+    return ffi.new_handle(handle_id)
