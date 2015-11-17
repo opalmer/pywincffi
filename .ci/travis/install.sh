3fb006a789fef94bb1c8da7dedf3f125ec538517
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-16 21:47:10 -0500

    need pylint < 1.4 for Python 2.6

--- .ci/travis/install.sh
+++ .ci/travis/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-if [[ $PYLINT == "1" ]]; then
+if [[ $PYLINT == "1" ]] && [[ $TRAVIS_PYTHON_VERSION == "2.6" ]]; then
+    pip install pylint<1.4 pep8
+
+elif [[ $PYLINT == "1" ]]; then
     pip install pylint pep8
+
 fi
 
 if [[ $READTHEDOCS == "1" ]]; then
