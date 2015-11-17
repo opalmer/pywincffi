0eaf507eb7f28983a7944e773362b4a9ef6c00ad
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-16 22:06:38 -0500

    skip pylint on Python 2.6
    pylint on Python 2.6 is no longer supported so skip
    it in the build.  However, given that we're still testing
    against it and building the docs for it we should be ok
    static analysis wise.

--- .ci/travis/install.sh
+++ .ci/travis/install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
 # Required for building docs and static analysis.
-pip install unittest2 pep8 sphinx
-
-if [[ $TRAVIS_PYTHON_VERSION == "2.6" ]]; then
-    pip install "pylint<1.4"
-else
-    pip install pylint
-fi
-
+pip install unittest2 pep8 sphinx pylint
 pip install .
