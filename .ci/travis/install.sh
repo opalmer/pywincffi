b7cd0d734facc28e55be47e961ed40ae064daea0
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-16 21:59:29 -0500

    simplified install.sh

--- .ci/travis/install.sh
+++ .ci/travis/install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
 # Required for building docs and static analysis.
-pip install unittest2
+pip install unittest2 pep8 sphinx
 
-if [[ $PYLINT == "1" ]] && [[ $TRAVIS_PYTHON_VERSION == "2.6" ]]; then
-    pip install pylint<1.4 pep8
-
-elif [[ $PYLINT == "1" ]]; then
-    pip install pylint pep8
-
-fi
-
-if [[ $READTHEDOCS == "1" ]]; then
-    pip install sphinx
+if [[ $TRAVIS_PYTHON_VERSION == "2.6" ]]; then
+    pip install pylint<1.4
+else
+    pip install pylint
 fi
 
 pip install .
