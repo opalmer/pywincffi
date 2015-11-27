f2af52c535a1aa8dcd60f85dd78338596b134f75
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-26 19:45:58 -0500

    adding VC redists to downloads

--- .ci/vagrant/download.ps1
+++ .ci/vagrant/download.ps1
@@ -23,10 +23,13 @@ function DownloadPythonInstallers {
     Download https://www.python.org/ftp/python/$version/python-$version.amd64.msi C:\provision\python\$version-x64.msi
 }
 
-
-Download https://bootstrap.pypa.io/ez_setup.py C:\provision\python\ez_setup.py
 DownloadPythonInstallers 2.6.6
 DownloadPythonInstallers 2.7.10
 DownloadPythonInstallers 3.4.3
-Download "http://download.microsoft.com/download/8/B/5/8B5804AD-4990-40D0-A6AA-CE894CBBB3DC/VS2008ExpressENUX1397868.iso" C:\provision\visual_studio\VS2008ExpressENUX1397868.iso
-Download "http://download.microsoft.com/download/1/E/5/1E5F1C0A-0D5B-426A-A603-1798B951DDAE/VS2010Express1.iso" C:\provision\visual_studio\VS2010Express1.iso
+Download "https://bootstrap.pypa.io/ez_setup.py" "C:\provision\python\ez_setup.py"
+Download "https://download.microsoft.com/download/1/1/1/1116b75a-9ec3-481a-a3c8-1777b5381140/vcredist_x86.exe" "C:\provision\vc_redist\2008x86.exe"
+Download "https://download.microsoft.com/download/d/2/4/d242c3fb-da5a-4542-ad66-f9661d0a8d19/vcredist_x64.exe" "C:\provision\vc_redist\2008x64.exe"
+Download "https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe" "C:\provision\vc_redist\2010x86.exe"
+Download "https://download.microsoft.com/download/3/2/2/3224B87F-CFA0-4E70-BDA3-3DE650EFEBA5/vcredist_x64.exe" "C:\provision\vc_redist\2010x64.exe"
+Download "https://download.microsoft.com/download/8/B/5/8B5804AD-4990-40D0-A6AA-CE894CBBB3DC/VS2008ExpressENUX1397868.iso" "C:\provision\visual_studio\VS2008ExpressENUX1397868.iso"
+Download "https://download.microsoft.com/download/1/E/5/1E5F1C0A-0D5B-426A-A603-1798B951DDAE/VS2010Express1.iso" "C:\provision\visual_studio\VS2010Express1.iso"
