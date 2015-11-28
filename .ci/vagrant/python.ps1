a1744f40f1bfc6254381faaa451bbd2f261bb936
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-27 20:20:40 -0500

    string fixes and swapping Start-Process with Run

--- .ci/vagrant/python.ps1
+++ .ci/vagrant/python.ps1
@@ -1,12 +1,5 @@
 . "c:\provision\scripts\functions.ps1"
 
-function DownloadInstallers {
-    $version = $args[0]
-
-    Download https://www.python.org/ftp/python/$version/python-$version.msi C:\provision\python\$version-x86.msi
-    Download https://www.python.org/ftp/python/$version/python-$version.amd64.msi C:\provision\python\$version-x64.msi
-}
-
 function Install {
     $version = $args[0]
     $install_path = "C:\python\$version"
@@ -17,32 +10,20 @@ function Install {
 
     if (!(Test-Path -Path $install_path )) {
         Write-Output "Installing Python $version"
-        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $msi_path TARGETDIR=$install_path ALLUSERS=1 ADDLOCAL=DefaultFeature" -Wait
+        Run "msiexec.exe" "/i $msi_path TARGETDIR=$install_path ALLUSERS=1 ADDLOCAL=DefaultFeature"
     } else {
         Write-Output "Python $version appears to be installed at $install_path"
     }
 
-    Write-Output "$python -m compileall $install_path"
-    Start-Process -FilePath "$python" -ArgumentList "-m compileall $install_path" -Wait
-
-    Write-Output "$python C:\provision\python\ez_setup.py"
-    Start-Process -FilePath "$python" -ArgumentList "C:\provision\python\ez_setup.py" -Wait
-
-    Write-Output "$easy_install pip"
-    Start-Process -FilePath "$easy_install" -ArgumentList "pip" -Wait
-
-    Write-Output "$pip virtualenv"
-    Start-Process -FilePath "$pip" -ArgumentList "virtualenv" -Wait
+    Run "$python" "-m compileall $install_path"
+    Run "$python" "C:\provision\python\ez_setup.py"
+    Run "$easy_install" "pip"
+    Run "$pip" "virtualenv"
 }
 
-Download https://bootstrap.pypa.io/ez_setup.py C:\provision\python\ez_setup.py
-DownloadInstallers 2.6.6
-DownloadInstallers 2.7.10
-DownloadInstallers 3.4.3
-
-Install 2.6.6-x86
-Install 2.6.6-x64
-Install 2.7.10-x86
-Install 2.7.10-x64
-Install 3.4.3-x86
-Install 3.4.3-x64
+Install "2.6.6-x86"
+Install "2.6.6-x64"
+Install "2.7.10-x86"
+Install "2.7.10-x64"
+Install "3.4.3-x86"
+Install "3.4.3-x64"
