f37f363caa0b803f0b428588227c43f3402f9802
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-28 16:46:01 -0500

    renaming script

--- .ci/vagrant/main.ps1
+++ .ci/vagrant/main.ps1
@@ -1,6 +1,6 @@
 . "C:\code\.ci\vagrant\functions.ps1"
 
-RunScript "C:\code\.ci\vagrant\firewall.ps1"
+RunScript "C:\code\.ci\vagrant\system.ps1"
 RunScript "C:\code\.ci\vagrant\download.ps1"
 RunScript "C:\code\.ci\vagrant\visual-studio.ps1"
 RunScript "C:\code\.ci\vagrant\cygwin.ps1"
