f357816623fb167315674edf52d07caee16fd5d7
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-28 11:34:42 -0500

    copying over authorized_keys if they exist

--- .ci/vagrant/sshd.ps1
+++ .ci/vagrant/sshd.ps1
@@ -8,4 +8,20 @@ if ($service.Length -eq 0) {
     Run "C:\cygwin\bin\bash.exe" $args
 }
 
+if (!(Test-Path -Path "C:\cygwin\home\users\vagrant\.ssh\" )) {
+    Write-Output "Creating ~/.ssh/"
+    Run "C:\cygwin\bin\bash.exe" "--login -c 'mkdir -p ~/.ssh/'"
+    Run "C:\cygwin\bin\bash.exe" "--login -c 'chmod 700 ~/.ssh/'"
+}
+
+if ((Test-Path -Path "C:\code\.ci\vagrant\files\authorized_keys" )) {
+    Write-Output "Copying C:\code\.ci\vagrant\files\authorized_keys to ~\.ssh\authorized_keys"
+    $src = "/cygdrive/c/code/.ci/vagrant/files/authorized_keys"
+    $dst = "/home/vagrant/.ssh/authorized_keys"
+    Run "C:\cygwin\bin\bash.exe" "--login -c 'cp $src $dst"
+    Run "C:\cygwin\bin\bash.exe" "--login -c 'chmod 644 $dst"
+} else {
+    Write-Output "C:\code\.ci\vagrant\files\authorized_keys does not exist"
+}
+
 Run "net.exe" "start sshd"
