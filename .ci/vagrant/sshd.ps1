781c29e484ed77fed2bf7537656411ca1b814c50
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-28 16:45:43 -0500

    fixing sshd setup

--- .ci/vagrant/sshd.ps1
+++ .ci/vagrant/sshd.ps1
@@ -1,25 +1,24 @@
 . "C:\code\.ci\vagrant\functions.ps1"
 
-# Setup sshd (needed for remote intrepreters)
-$service = Get-Service -Name sshd -ErrorAction SilentlyContinue
-if ($service.Length -eq 0) {
-    Write-Output "Configuring sshd"
-    $args = "--login -c '/bin/ssh-host-config--yes --name sshd --pwd vagrant'"
-    Run "C:\cygwin\bin\bash.exe" $args
-}
+$BASH = "C:\cygwin\bin\bash.exe"
+
+# Uninstall the existing service first if one exists.  This ensures we get
+# a clean slate every time.
+Run $BASH "--login -c '/usr/bin/cygrunsrv --remove sshd'"
+Run $BASH "--login -c '/bin/ssh-host-config --yes --name sshd --pwd 'vagrant'"
 
 if (!(Test-Path -Path "C:\cygwin\home\users\vagrant\.ssh\" )) {
     Write-Output "Creating ~/.ssh/"
-    Run "C:\cygwin\bin\bash.exe" "--login -c 'mkdir -p ~/.ssh/'"
-    Run "C:\cygwin\bin\bash.exe" "--login -c 'chmod 700 ~/.ssh/'"
+    Run $BASH "--login -c 'mkdir -p ~/.ssh/'"
+    Run $BASH "--login -c 'chmod 700 ~/.ssh/'"
 }
 
 if ((Test-Path -Path "C:\code\.ci\vagrant\files\authorized_keys" )) {
     Write-Output "Copying C:\code\.ci\vagrant\files\authorized_keys to ~\.ssh\authorized_keys"
     $src = "/cygdrive/c/code/.ci/vagrant/files/authorized_keys"
     $dst = "/home/vagrant/.ssh/authorized_keys"
-    Run "C:\cygwin\bin\bash.exe" "--login -c 'cp $src $dst"
-    Run "C:\cygwin\bin\bash.exe" "--login -c 'chmod 644 $dst"
+    Run $BASH "--login -c 'cp $src $dst"
+    Run $BASH "--login -c 'chmod 644 $dst"
 } else {
     Write-Output "C:\code\.ci\vagrant\files\authorized_keys does not exist"
 }
@@ -28,7 +27,7 @@ if ((Test-Path -Path "C:\code\.ci\vagrant\files\authorized_keys" )) {
 Write-Output "Replace SSHD configuration"
 $src = "/cygdrive/c/code/.ci/vagrant/files/sshd_config"
 $dst = "/etc/sshd_config"
-Run "C:\cygwin\bin\bash.exe" "--login -c 'cp $src $dst"
+Run $BASH "--login -c 'cp $src $dst"
 
 RestartService "sshd"
 
