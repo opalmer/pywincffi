4072b56e2f163b5ddb8511728181e01f4be9ab6e
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-27 20:17:29 -0500

    create common functions

---
+++ .ci/vagrant/functions.ps1
@@ -0,0 +1,45 @@
+function Download($url, $output) {
+    $parent_dir = Split-Path $output -parent
+    $start_time = Get-Date
+
+    if (!(Test-Path -Path $parent_dir )) {
+        Write-Output "Creating $parent_dir"
+        New-Item -ItemType directory -Path $parent_dir
+    }
+
+    if (!(Test-Path -Path $output )) {
+        Write-Output "Downloading $url to $output"
+        $client = New-Object System.Net.WebClient
+        $client.DownloadFile($url, $output)
+    } else {
+        Write-Output "Already downloaded $url"
+    }
+}
+
+function RunScript($script) {
+    Write-Output "[starting] $script"
+    & $script
+    Write-Output "[finished] $script"
+}
+
+function Run($filename, $arguments) {
+    Write-Output "run: $filename $arguments"
+    $start_info = New-object System.Diagnostics.ProcessStartInfo
+    $start_info.CreateNoWindow = $true
+    $start_info.UseShellExecute = $false
+    $start_info.RedirectStandardOutput = $true
+    $start_info.RedirectStandardError = $true
+    $start_info.FileName = $filename
+    $start_info.Arguments = $arguments
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $start_info
+    [void]$process.Start()
+    $output = $process.StandardOutput.ReadToEnd()
+    $process.WaitForExit()
+    $code = $process.ExitCode
+    $output
+
+    # We don't really care about the exit code in most
+    # cases so we'll just print it.
+    Write-Output "exit code: $code"
+}
