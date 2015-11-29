294257bab087f15354656c7c2fff203286aa036f
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-29 10:51:33 -0500

    adding virtualenv setup

---
+++ .ci/vagrant/virtualenv.ps1
@@ -0,0 +1,12 @@
+. "C:\code\.ci\vagrant\functions.ps1"
+
+$BASH = "C:\cygwin\bin\bash.exe"
+$CODE = "/cygdrive/c/code/"
+$items = Get-ChildItem C:\python -name
+
+Run $BASH "--login -c 'mkdir -p /cygdrive/c/virtualenv'"
+
+foreach ($pyversion in $items) {
+    $virtualenv = "/cygdrive/c/python/$pyversion/Scripts/virtualenv.exe"
+    Run $BASH "--login -c '$virtualenv virtualenv/$pyversion --clear'"
+}
