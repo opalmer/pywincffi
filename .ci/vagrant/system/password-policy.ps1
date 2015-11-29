c2282e697581fc20ee518882418d0e5b6ee11e55
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-29 17:53:34 -0500

    adding some missing descriptions

--- .ci/vagrant/system/password-policy.ps1
+++ .ci/vagrant/system/password-policy.ps1
@@ -1,5 +1,9 @@
 . "C:\code\.ci\vagrant\functions.ps1"
 
-Write-Output "Changing password policy"
+#
+# Changes the password policies so we don't run into
+# issues when using the default 'vagrant' password.
+#
+
 SafeRun "secedit.exe" "/configure /db C:\windows\security\local.sdb /cfg C:\code\.ci\vagrant\system\files\secpol.cfg /areas SECURITYPOLICY"
 SafeRun "gpupdate.exe" "/Force"
--- .ci/vagrant/system/uac.ps1
+++ .ci/vagrant/system/uac.ps1
@@ -1,6 +1,10 @@
 . "C:\code\.ci\vagrant\functions.ps1"
 
-Write-Output "Disabling UAC (may require a reboot)"
+#
+# Disables User Account Control (UAC) which will prevent
+# certain actions from executing in an unattended fashion.
+#
+
 New-ItemProperty `
     -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system `
     -Name EnableLUA -PropertyType DWord -Value 0 -Force
