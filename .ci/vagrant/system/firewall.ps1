9a3cefb81efa5f3662bbd91482714b06602eba2d
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-29 18:18:12 -0500

    adding missing firewall port

--- .ci/vagrant/system/firewall.ps1
+++ .ci/vagrant/system/firewall.ps1
@@ -8,3 +8,4 @@
 SafeRun "netsh.exe" 'advfirewall firewall add rule name="SSHD" dir=in action=allow protocol=TCP localport=22'
 SafeRun "netsh.exe" 'advfirewall firewall add rule name="Remote Desktop" dir=in action=allow protocol=TCP localport=3389'
 SafeRun "netsh.exe" 'advfirewall firewall add rule name="WinRM" dir=in action=allow protocol=TCP localport=5985'
+SafeRun "netsh.exe" 'advfirewall firewall add rule name="WinRM" dir=in action=allow protocol=TCP localport=5986'
