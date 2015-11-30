997cd8c986b378eb0b7f33f5633f09cbe9d11901
Oliver Palmer <oliverpalmer@opalmer.com>
2015-11-29 20:15:57 -0500

    adding missing package 'rsync'

--- .ci/vagrant/software/cygwin.ps1
+++ .ci/vagrant/software/cygwin.ps1
@@ -8,7 +8,7 @@ if (!(Test-Path -Path "C:\cygwin")) {
     Download "https://cygwin.com/setup-x86_64.exe" "C:\provision\cygwin\setup-x86_64.exe"
 
     Write-Output "Installing cygwin"
-    $args = "--quiet-mode --verbose --no-shortcuts --no-desktop --local-package-dir C:\provision\cygwin\packages\ --root C:\cygwin\ --site http://mirrors.kernel.org/sourceware/cygwin/ --packages openssh,bash"
+    $args = "--quiet-mode --verbose --no-shortcuts --no-desktop --local-package-dir C:\provision\cygwin\packages\ --root C:\cygwin\ --site http://mirrors.kernel.org/sourceware/cygwin/ --packages openssh,bash,rsync"
     Run "C:\provision\cygwin\setup-x86_64.exe" $args
 
 } else {
