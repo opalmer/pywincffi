09b9248e012bdf2f58b38332bea48442b4ee0410
Oliver Palmer <oliverpalmer@opalmer.com>
2016-01-10 10:42:29 -0500

    adding get_appveyor_build and fixing some bugs

--- pywincffi/dev/release.py
+++ pywincffi/dev/release.py
@@ -22,26 +22,48 @@ logger = get_logger("dev.release")
 APPVEYOR_API = "https://ci.appveyor.com/api"
 APPVEYOR_API_PROJ = APPVEYOR_API + "/projects/opalmer/pywincffi"
 
+
 def _save_response_content(response, path):
     """
     Given a response object from the requests library, iterate over the
     content and save it to ``path``.
     """
     assert isinstance(response, requests.Response)
+
     with open(path, "wb") as file_:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:
                 file_.write(chunk)
 
 
-def download_build_artifacts(output, jobs):
+def get_appveyor_build(branch):
+    """
+    Returns information about the latest build for the
+    requested ``branch``
+    """
+    url = APPVEYOR_API_PROJ + "/branch/master"
+    response = requests.get(url)
+
+    if not response.ok:
+        raise RuntimeError(
+            "Expected a 200 response for GET %s.  Got %s instead." % (
+                url, response.status_code))
+
+    return response.json()
+
+
+def download_build_artifacts(output_dir, jobs):
     """
     Given a build from AppVeyor download the build artifacts
-    to the given ``output``.
+    to the given ``output``.  This function expects ``jobs``
+    to contain the job list:
+
+    >>> data = get_appveyor_build("master")
+    >>> download_build_artifacts("tmp", data["build"]["jobs"])
     """
     assert isinstance(jobs, list)
     try:
-        os.makedirs(output)
+        os.makedirs(output_dir)
     except (OSError, IOError) as error:
         if error.errno != EEXIST:
             raise
@@ -49,18 +71,18 @@ def download_build_artifacts(output, jobs):
     paths = []
 
     # Locate the build artifacts and download them
-    logger.debug("Downloading build artifacts to %s", output)
+    logger.debug("Downloading build artifacts to %s", output_dir)
     for job in jobs:
         job_id = job["jobId"]
         if job["status"] != "success":
-            print("... status of job %s != success" % job_id)
-            raise Exception("Cannot publish a failed job.")
+            raise RuntimeError(
+                "Cannot publish a failed job. (%r != success)" % job["status"])
 
         # Iterate over and download all the artifacts
         artifact_url = APPVEYOR_API + "/buildjobs/%s/artifacts" % job_id
         build_artifacts = requests.get(artifact_url).json()
         if not build_artifacts:
-            raise Exception(
+            raise RuntimeError(
                 "Build %s does not contain any artifacts" % artifact_url)
 
         for artifact in build_artifacts:
@@ -69,8 +91,8 @@ def download_build_artifacts(output, jobs):
                 continue
 
             file_url = artifact_url + "/%s" % artifact["fileName"]
-            logger.info("Download and unpack", file_url)
-            local_path = join(output, basename(artifact["fileName"]))
+            logger.info("Download and unpack %s", file_url)
+            local_path = join(output_dir, basename(artifact["fileName"]))
             response = requests.get(
                 file_url, stream=True, headers={"Content-Type": ""})
 
