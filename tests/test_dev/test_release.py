9f803b3771c62d940b0e3d613717688482ec3be2
Oliver Palmer <oliverpalmer@opalmer.com>
2016-01-13 22:15:34 -0500

    tests for release.Session

--- tests/test_dev/test_release.py
+++ tests/test_dev/test_release.py
@@ -1,10 +1,19 @@
 import os
+import hashlib
 import tempfile
 import subprocess
 import sys
 from os.path import dirname, abspath, isfile, join
 
-from pywincffi.dev.release import check_wheel
+try:
+    from http.client import OK, BAD_REQUEST
+except ImportError:
+    # pylint: disable=import-error,wrong-import-order
+    from httplib import OK, BAD_REQUEST
+
+from requests.adapters import HTTPAdapter
+
+from pywincffi.dev.release import Session, AppVeyor, check_wheel
 from pywincffi.dev.testutil import TestCase
 
 
@@ -29,16 +38,72 @@ class TestWheel(TestCase):
         setup_py = join(root_dir, "setup.py")
         self.assertTrue(isfile(setup_py), "%s does not exist" % setup_py)
 
-        # Build pywincffi.  We should always be able to unpack our own
-        # library and this also gives us a way to test wheel build directly.
-        process = subprocess.Popen(
-            [sys.executable, "setup.py", "bdist_wheel"],
-            stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=root_dir)
-        process.communicate()
-        self.assertEqual(process.returncode, 0)
+        wheels = []
 
-        for root, _, files in os.walk(join(root_dir, "dist")):
-            for filename in files:
-                if filename.endswith(".whl"):
-                    path = join(root, filename)
-                    self.assertTrue(check_wheel(path), path)
+        while not wheels:
+            for root, _, files in os.walk(join(root_dir, "dist")):
+                for filename in files:
+                    if filename.endswith(".whl"):
+                        wheels.append(join(root, filename))
+
+            if wheels:
+                break
+
+            # Build pywincffi if we need to. We should always be able to
+            # unpack our own library.
+            process = subprocess.Popen(
+                [sys.executable, "setup.py", "bdist_wheel"],
+                stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=root_dir)
+            process.communicate()
+            self.assertEqual(process.returncode, 0)
+
+        for path in wheels:
+            self.assertTrue(check_wheel(path), path)
+
+
+class TestSession(TestCase):
+    DOWNLOAD_SHA1 = "89ff14348b410051fff2eb206183993f659d85e0"
+    DOWNLOAD_URL = "https://raw.githubusercontent.com/opalmer/pywincffi/" \
+                    "master/.ci/appveyor/run_with_compiler.cmd"
+
+    def setUp(self):
+        super(TestSession, self).setUp()
+        self.session = Session.session
+        self.session.mount("https://", HTTPAdapter(max_retries=100))
+
+    def test_check_code_success(self):
+        response = self.session.get(AppVeyor.API)
+        Session.check_code(response, OK)
+
+    def test_check_code_failure(self):
+        response = self.session.get(AppVeyor.API)
+
+        with self.assertRaises(RuntimeError):
+            Session.check_code(response, BAD_REQUEST)
+
+    def test_json_success(self):
+        data = Session.json(AppVeyor.API_PROJECT)
+        self.assertEqual(data.get("project", {}).get("name", {}), "pywincffi")
+
+    def test_json_failure(self):
+        with self.assertRaises(ValueError):
+            Session.json(AppVeyor.API)
+
+    def test_download_random_path(self):
+        path = Session.download(self.DOWNLOAD_URL)
+        self.addCleanup(os.remove, path)
+        with open(path, "rb") as file_:
+            sha1 = hashlib.sha1(file_.read())
+
+        self.assertEqual(sha1.hexdigest(), self.DOWNLOAD_SHA1)
+
+    def test_download_specific_path(self):
+        fd, path = tempfile.mkstemp()
+        os.close(fd)
+        self.addCleanup(os.remove, path)
+
+        Session.download(self.DOWNLOAD_URL, path=path)
+        with open(path, "rb") as file_:
+            sha1 = hashlib.sha1(file_.read())
+
+        self.assertEqual(sha1.hexdigest(), self.DOWNLOAD_SHA1)
