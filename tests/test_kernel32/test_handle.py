9c132ee06fa416930bfe6781b8874022f49eabd4
Oliver Palmer <oliverpalmer@opalmer.com>
2016-01-30 18:23:35 -0500

    adding tests for WaitForSingleObject()

--- tests/test_kernel32/test_handle.py
+++ tests/test_kernel32/test_handle.py
@@ -4,8 +4,10 @@ from errno import EBADF
 
 from pywincffi.core import dist
 from pywincffi.dev.testutil import TestCase
-from pywincffi.exceptions import InputError
-from pywincffi.kernel32 import GetStdHandle, CloseHandle, handle_from_file
+from pywincffi.exceptions import InputError, WindowsAPIError
+from pywincffi.kernel32 import (
+    GetStdHandle, CloseHandle, OpenProcess, WaitForSingleObject,
+    handle_from_file)
 
 try:
     WindowsError
@@ -81,5 +83,40 @@ class TestWaitForSingleObject(TestCase):
     """
     Tests for :func:`pywincffi.kernel32.WaitForSingleObject`
     """
-    # TODO: test for WAIT_FAILED
-    # TODO: pass in a process handle without SYNCHRONIZE
+    def test_wait_failed(self):
+        # This should cause WAIT_FAILED to be returned by the underlying
+        # WaitForSingleObject because we didn't request the SYNCHRONIZE
+        # permission.
+        process = self.create_python_process("import time; time.sleep(3)")
+        _, library = dist.load()
+
+        hProcess = OpenProcess(
+            library.PROCESS_QUERY_INFORMATION, False, process.pid)
+        self.addCleanup(CloseHandle, hProcess)
+
+        with self.assertRaises(WindowsAPIError) as exec_:
+            WaitForSingleObject(hProcess, 3)
+            self.assertEqual(exec_.code, library.WAIT_FAILED)
+
+    def test_wait_on_running_process(self):
+        process = self.create_python_process("import time; time.sleep(1)")
+        _, library = dist.load()
+
+        hProcess = OpenProcess(
+            library.PROCESS_QUERY_INFORMATION | library.SYNCHRONIZE,
+            False, process.pid)
+        self.addCleanup(CloseHandle, hProcess)
+        self.assertEqual(
+            WaitForSingleObject(hProcess, 0), library.WAIT_TIMEOUT)
+
+    def test_process_dies_before_timeout(self):
+        process = self.create_python_process("import time; time.sleep(1)")
+        _, library = dist.load()
+
+        hProcess = OpenProcess(
+            library.PROCESS_QUERY_INFORMATION | library.SYNCHRONIZE,
+            False, process.pid)
+        self.addCleanup(CloseHandle, hProcess)
+        self.assertEqual(
+            WaitForSingleObject(hProcess, library.INFINITE),
+            library.WAIT_OBJECT_0)
