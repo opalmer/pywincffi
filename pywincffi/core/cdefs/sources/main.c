c743414d962b7fbb1b7618942359e0b5197fb0d5
Oliver Palmer <oliverpalmer@opalmer.com>
2016-02-03 21:29:41 -0500

    need to remove the constatnt from main.c too

--- pywincffi/core/cdefs/sources/main.c
+++ pywincffi/core/cdefs/sources/main.c
@@ -4,7 +4,6 @@
 // Extra constants which are not defined in all versions of Windows or
 // that pywincffi creates.
 static const int FILE_FLAG_SESSION_AWARE = 0x00800000;
-static const int INHERIT_PARENT_AFFINITY = 0x00010000;
 
 HANDLE handle_from_fd(int fd) {
     return (HANDLE)_get_osfhandle(fd);
