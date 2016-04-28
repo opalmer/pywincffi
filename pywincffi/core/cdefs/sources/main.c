6b5c1768470602483558635c099e51922a350621
Oliver Palmer <oliverpalmer@opalmer.com>
2016-04-27 22:44:05 -0400

    adding constants for _STARTUPINFO

--- pywincffi/core/cdefs/sources/main.c
+++ pywincffi/core/cdefs/sources/main.c
@@ -4,6 +4,7 @@
 // Extra constants which are not defined in all versions of Windows or
 // that pywincffi creates.
 static const int FILE_FLAG_SESSION_AWARE = 0x00800000;
+static const int STARTF_UNTRUSTEDSOURCE = 0x00008000;
 
 HANDLE handle_from_fd(int fd) {
     return (HANDLE)_get_osfhandle(fd);
