070c5aea39b555eb3d0c65d674b5441094a7d4fc
opalmer <oliverpalmer@opalmer.com>
2016-05-02 22:11:45 -0400

    define the constant if it does not exist (#82)

--- pywincffi/core/cdefs/sources/main.c
+++ pywincffi/core/cdefs/sources/main.c
@@ -1,10 +1,28 @@
 #include <io.h>
 #include <windows.h>
 
-// Extra constants which are not defined in all versions of Windows or
-// that pywincffi creates.
-static const int FILE_FLAG_SESSION_AWARE = 0x00800000;
-static const int STARTF_UNTRUSTEDSOURCE = 0x00008000;
+// Extra constants which are not defined in all versions of the Windows
+// SDK.  If cffi fails to find the value, it ends up being picked up from
+// here.
+#if !defined(FILE_FLAG_SESSION_AWARE)
+    static const int FILE_FLAG_SESSION_AWARE = 0x00800000;
+#endif
+
+#if !defined(STARTF_UNTRUSTEDSOURCE)
+    static const int STARTF_UNTRUSTEDSOURCE = 0x00008000;
+#endif
+
+#if !defined(STARTF_PREVENTPINNING)
+    static const int STARTF_PREVENTPINNING = 0x00002000;
+#endif
+
+#if !defined(STARTF_TITLEISAPPID)
+    static const int STARTF_TITLEISAPPID = 0x00001000;
+#endif
+
+#if !defined(STARTF_TITLEISLINKNAME)
+    static const int STARTF_TITLEISLINKNAME = 0x00000800;
+#endif
 
 HANDLE handle_from_fd(int fd) {
     return (HANDLE)_get_osfhandle(fd);
