65d3a0ef55fbd25a884f50a24c9fc26aa8ae9073
Oliver Palmer <oliverpalmer@opalmer.com>
2016-05-05 00:13:13 -0400

    initial commit of WSAEventSelect()

---
+++ pywincffi/ws3_32/__init__.py
@@ -0,0 +1,9 @@
+"""
+Ws2_32 Sub-Package
+==================
+
+Provides functions, constants and utilities that wrap functions provided by
+``ws3_32.dll``.
+"""
+
+from pywincffi.ws3_32.events import WSAEventSelect
---
+++ pywincffi/ws3_32/events.py
@@ -0,0 +1,53 @@
+"""
+Events
+------
+
+A module containing Windows functions for working with events.
+"""
+
+from six import integer_types
+
+from pywincffi.core import dist
+from pywincffi.core.checks import Enums, input_check, error_check
+from pywincffi.exceptions import WindowsAPIError
+
+
+def WSAEventSelect(sock, hEventObject, lNetworkEvents):
+    """
+    Specifies an event object to be associated with the specified set of
+    FD_XXX network events.
+
+    .. seealso::
+
+        https://msdn.microsoft.com/en-us/library/ms741576
+
+    :param int sock:
+        A descriptor identify the socket.
+
+    :param handle hEventObject:
+        A handle which identifies the event object to be associated
+        with the network events.
+
+    :param int lNetworkEvents:
+        A bitmask which specifies the combination of ``FD_XXX`` network
+        events which the application has interest in.
+    """
+    input_check("sock", sock, integer_types)
+    input_check("hEventObject", hEventObject, Enums.HANDLE)
+    input_check("lNetworkEvents", lNetworkEvents, integer_types)
+
+    ffi, library = dist.load()
+
+    # TODO: `sock` needs conversion
+    code = library.WSAEventSelect(
+        sock,
+        ffi.cast("WSAEVENT", hEventObject),
+        ffi.cast("long", lNetworkEvents)
+    )
+
+    if code == library.SOCKET_ERROR:
+        errno = library.WSAGetLastError()
+        raise WindowsAPIError(
+            "WSAEventSelect", "Socket error %d" % errno, errno)
+
+    error_check("WSAEventSelect", code, expected=0)
