ab4c3768a472c5fbb56447cfdfbfd2a62025cf40
exvito <ex.vitorino@gmail.com>
2016-05-14 00:29:56 +0100

    Types - Creating wintypes package and associated tests

---
+++ pywincffi/wintypes/__init__.py
@@ -0,0 +1,53 @@
+"""
+Windows Types Package
+====================
+
+Provides user accessible types corresponding to the respective Windows types
+used across the exposed APIs.
+"""
+
+
+from pywincffi.core import typesbase
+from pywincffi.core import dist
+
+
+_ffi, _ = dist.load()
+
+
+class SECURITY_ATTRIBUTES(typesbase.CFFICDataWrapper):
+    """
+    .. seealso: https://msdn.microsoft.com/en-us/library/aa379560
+    """
+    def __init__(self):
+        super(SECURITY_ATTRIBUTES, self).__init__(
+            "SECURITY_ATTRIBUTES*",
+            _ffi,
+        )
+        self._cdata.nLength = _ffi.sizeof(self._cdata)
+
+    @property
+    def nLength(self):
+        return self._cdata.nLength
+
+    @nLength.setter
+    def nLength(self, _):
+        # Can't raise AttributeError.
+        # CFFICDataWrapper would fall back to setting self._cdata.nLength .
+        raise TypeError('SECURITY_ATTRIBUTES.nLength is read-only')
+
+
+class OVERLAPPED(typesbase.CFFICDataWrapper):
+    """
+    .. seealso: https://msdn.microsoft.com/en-us/library/ms684342
+    """
+    def __init__(self):
+        super(OVERLAPPED, self).__init__("OVERLAPPED*", _ffi)
+
+
+class FILETIME(typesbase.CFFICDataWrapper):
+    """
+    .. seealso: https://msdn.microsoft.com/en-us/library/ms724284
+    """
+    def __init__(self):
+        super(FILETIME, self).__init__("FILETIME*", _ffi)
+
---
+++ tests/test_wintypes/__init__.py
@@ -0,0 +1,72 @@
+from pywincffi import wintypes
+from pywincffi.dev.testutil import TestCase
+
+
+class Test_SECURITY_ATTRIBUTES(TestCase):
+    """
+    Tests for :class:`pywincffi.wintypes.SECURITY_ATTRIBUTES`
+    """
+    def test_attr_nLength(self):
+        sa = wintypes.SECURITY_ATTRIBUTES()
+        self.assertGreater(sa.nLength, 0)
+
+    def test_attr_nLength_read_only(self):
+        sa = wintypes.SECURITY_ATTRIBUTES()
+        with self.assertRaises(TypeError):
+            sa.nLength = 1
+
+    def test_attr_bInheritHandle_true(self):
+        sa = wintypes.SECURITY_ATTRIBUTES()
+        sa.bInheritHandle = True
+        self.assertEqual(sa.bInheritHandle, True)
+
+    def test_attr_bInheritHandle_false(self):
+        sa = wintypes.SECURITY_ATTRIBUTES()
+        sa.bInheritHandle = False
+        self.assertEqual(sa.bInheritHandle, False)
+
+    def test_missing_attr(self):
+        sa = wintypes.SECURITY_ATTRIBUTES()
+        with self.assertRaises(AttributeError):
+            sa.no_such_attr = None
+
+
+class Test_OVERLAPPED(TestCase):
+    """
+    Tests for :class:`pywincffi.wintypes.OVERLAPPED`
+    """
+    def test_attr_Offset(self):
+        o = wintypes.OVERLAPPED()
+        o.Offset = 12345
+        self.assertEqual(o.Offset, 12345)
+
+    def test_attr_OffsetHigh(self):
+        o = wintypes.OVERLAPPED()
+        o.OffsetHigh = 54321
+        self.assertEqual(o.OffsetHigh, 54321)
+
+    def test_missing_attr(self):
+        o = wintypes.OVERLAPPED()
+        with self.assertRaises(AttributeError):
+            o.no_such_attr = None
+
+
+class Test_FILETIME(TestCase):
+    """
+    Tests for :class:`pywincffi.wintypes.FILETIME`
+    """
+    def test_attr_dwLowDateTime(self):
+        ft = wintypes.FILETIME()
+        ft.dwLowDateTime = 42
+        self.assertEqual(ft.dwLowDateTime, 42)
+
+    def test_attr_dwHighDateTime(self):
+        ft = wintypes.FILETIME()
+        ft.dwHighDateTime = 24
+        self.assertEqual(ft.dwHighDateTime, 24)
+
+    def test_missing_attr(self):
+        ft = wintypes.FILETIME()
+        with self.assertRaises(AttributeError):
+            ft.no_such_attr = None
+
