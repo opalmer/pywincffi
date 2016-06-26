fe17b8ed37658ccdc84bad0da654650fd8f6a1b3
TurBoss <j.l.toledano.l@gmail.com>
2016-06-27 01:09:55 +0200

    Deletes test

--- tests/test_kernel32/test_process.py
+++ tests/test_kernel32/test_process.py
@@ -252,8 +252,3 @@ class TestCreateToolhelp32Snapshot(TestCase):
         handle = CreateToolhelp32Snapshot(library.TH32CS_SNAPPROCESS, 0)
         self.addCleanup(CloseHandle, handle)
 
-    def test_invalid_process(self):
-        _, library = dist.load()
-
-        with self.assertRaises(WindowsAPIError):
-            CreateToolhelp32Snapshot(library.TH32CS_SNAPPROCESS, 3)
