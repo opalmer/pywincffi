ef024e44d37503cc74c59ce9a71536782b01ca58
Oliver Palmer <oliverpalmer@opalmer.com>
2016-06-27 12:17:20 -0400

    created mixin for __eq__/__repr__ methods since they'll be shared with other classes

--- pywincffi/wintypes/objects.py
+++ pywincffi/wintypes/objects.py
@@ -11,8 +11,27 @@ from pywincffi.core import dist
 from pywincffi.core.typesbase import CFFICDataWrapper
 
 
+class ObjectMixin(object):  # pylint: disable=too-few-public-methods
+    """A mixin which is used by object classes to share common code."""
+    def __repr__(self):
+        ffi, _ = dist.load()
+        return "<%s 0x%x at 0x%x>" % (
+            self.__class__.__name__,
+            int(ffi.cast("intptr_t", self._cdata[0])),
+            id(self)
+        )
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError(
+                "%r must be a %s object" % (other, self.__class__.__name__))
+
+        # pylint: disable=protected-access
+        return self._cdata[0] == other._cdata[0]
+
+
 # pylint: disable=too-few-public-methods,protected-access
-class HANDLE(CFFICDataWrapper):
+class HANDLE(CFFICDataWrapper, ObjectMixin):
     """
     .. seealso::
 
@@ -21,20 +40,9 @@ class HANDLE(CFFICDataWrapper):
     def __init__(self, data=None):
         ffi, _ = dist.load()
         super(HANDLE, self).__init__("HANDLE[1]", ffi)
+
         # Initialize from a <cdata handle> object as returned by some
         # Windows API library calls: Python AND FFI types must be equal.
         if isinstance(data, type(self._cdata[0])):
             if ffi.typeof(data) == ffi.typeof(self._cdata[0]):
                 self._cdata[0] = data
-
-    def __repr__(self):
-        ffi, _ = dist.load()
-        return "<HANDLE 0x%x at 0x%x>" % (
-            int(ffi.cast("intptr_t", self._cdata[0])),
-            id(self)
-        )
-
-    def __eq__(self, other):
-        if not isinstance(other, HANDLE):
-            raise TypeError("%r must be a HANDLE" % other)
-        return self._cdata[0] == other._cdata[0]
