830b94c1a55d6806d707756ba8ad126912dc4d3b
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-01 08:27:34 -0400

    continued work on tests

--- tests/test_ws2_32/test_events.py
+++ tests/test_ws2_32/test_events.py
@@ -1,7 +1,11 @@
+import socket
+
+from pywincffi.core import dist
 from pywincffi.dev.testutil import TestCase, mock_library
 from pywincffi.exceptions import WindowsAPIError
 from pywincffi.kernel32 import CloseHandle
-from pywincffi.ws2_32 import WSAGetLastError, WSACreateEvent
+from pywincffi.ws2_32 import WSAGetLastError, WSACreateEvent, WSAEventSelect
+from pywincffi.wintypes import socket_from_object, wintype_to_cdata
 
 
 class TestWSAGetLastError(TestCase):
@@ -31,8 +35,18 @@ class TestWSAEventSelect(TestCase):
     """
     Tests for ``pywincffi.ws2_32.events.WSAEventSelect``
     """
-    # def test_select_event_call(self):
-    #     ffi, library = dist.load()
-    #     event = CreateEvent(
-    #         False, False, lpEventAttributes=None, lpName=None)
-    #     self.addCleanup(CloseHandle, event)
+    def create_socket(self):
+        _, library = dist.load()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.addCleanup(sock.close)
+        return socket_from_object(sock)
+
+    def test_select_event_call(self):
+        sock = self.create_socket()
+        ffi, library = dist.load()
+        event = WSACreateEvent()
+        self.addCleanup(CloseHandle, event)
+        WSAEventSelect(
+            sock, event,
+            library.FD_READ | library.FD_WRITE
+        )
