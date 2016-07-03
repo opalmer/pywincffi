2632e70c53b13275afda536db6d351308177cd5d
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-03 11:35:22 -0400

    lint fix

--- tests/test_ws2_32/test_events.py
+++ tests/test_ws2_32/test_events.py
@@ -5,7 +5,7 @@ from pywincffi.dev.testutil import TestCase, mock_library
 from pywincffi.exceptions import WindowsAPIError
 from pywincffi.kernel32 import CloseHandle
 from pywincffi.ws2_32 import WSAGetLastError, WSACreateEvent, WSAEventSelect
-from pywincffi.wintypes import socket_from_object, wintype_to_cdata
+from pywincffi.wintypes import socket_from_object
 
 
 class TestWSAGetLastError(TestCase):
@@ -36,14 +36,13 @@ class TestWSAEventSelect(TestCase):
     Tests for ``pywincffi.ws2_32.events.WSAEventSelect``
     """
     def create_socket(self):
-        _, library = dist.load()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.addCleanup(sock.close)
         return socket_from_object(sock)
 
     def test_select_event_call(self):
         sock = self.create_socket()
-        ffi, library = dist.load()
+        _, library = dist.load()
         event = WSACreateEvent()
         self.addCleanup(CloseHandle, event)
         WSAEventSelect(
