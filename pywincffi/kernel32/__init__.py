d93bf5121a49b7ea37875a64b6cf746ee31a30c7
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-04 16:06:11 -0400

    don't export environment_to_string since it's mainly meant for internal use

--- pywincffi/kernel32/__init__.py
+++ pywincffi/kernel32/__init__.py
@@ -19,8 +19,7 @@ from pywincffi.kernel32.pipe import (
     CreatePipe, PeekNamedPipe, PeekNamedPipeResult, SetNamedPipeHandleState)
 from pywincffi.kernel32.process import (
     GetProcessId, GetCurrentProcess, OpenProcess, GetExitCodeProcess,
-    pid_exists, TerminateProcess, CreateToolhelp32Snapshot,
-    CreateProcess, environment_to_string)
+    TerminateProcess, CreateToolhelp32Snapshot, CreateProcess, pid_exists)
 from pywincffi.kernel32.events import CreateEvent, OpenEvent, ResetEvent
 from pywincffi.kernel32.comms import ClearCommError
 from pywincffi.kernel32.synchronization import WaitForSingleObject
