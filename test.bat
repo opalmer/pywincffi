b62d140eddbb926c63bcc6c5c4e12965715ecc85
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-10 23:57:30 -0400

    build wheel first
    That way of the build of the wheel
    fails we won't encounter the build issues
    in the tests.

--- test.bat
+++ test.bat
@@ -33,6 +33,12 @@ pylint tests --reports no ^
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 ECHO ========================================================================================
+ECHO setup.py bdist_wheel
+ECHO ========================================================================================
+python setup.py bdist_wheel > NUL
+IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+ECHO ========================================================================================
 ECHO sphinx-build -q -b html -W -E -a -d docs/build/doctrees docs/source docs/build/html
 ECHO ========================================================================================
 sphinx-build -q -b html -W -E -a -d docs/build/doctrees docs/source docs/build/html
@@ -44,11 +50,6 @@ ECHO ===========================================================================
 sphinx-build -q -b linkcheck -W -E -a -d docs/build/doctrees docs/source docs/build/html
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO ========================================================================================
-ECHO setup.py bdist_wheel
-ECHO ========================================================================================
-python setup.py bdist_wheel > NUL
-IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 ECHO ========================================================================================
 ECHO nosetests -sv tests
