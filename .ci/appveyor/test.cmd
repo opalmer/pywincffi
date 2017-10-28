d2e50917666230ae3c611247b6f9b0140d085e66
Oliver Palmer <oliverpalmer@opalmer.com>
2017-10-28 12:54:05 -0400

    exclude gcov

--- .ci/appveyor/test.cmd
+++ .ci/appveyor/test.cmd
@@ -1,3 +1,3 @@
 %WITH_COMPILER% %PYTHON%\Scripts\pip.exe install -r dev_requirements.txt --upgrade --quiet || EXIT 1
 %WITH_COMPILER% %PYTHON%\Scripts\py.test.exe --strict --verbose --cov pywincffi --cov-report=xml || EXIT 1
-%PYTHON%\Scripts\codecov.exe -f coverage.xml --required || EXIT 1
+%PYTHON%\Scripts\codecov.exe -X gcov -f coverage.xml --required || EXIT 1
