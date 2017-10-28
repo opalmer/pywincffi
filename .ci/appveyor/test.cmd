798c8281a94647ffafdb48d69a062ec1c8a2032f
Oliver Palmer <oliverpalmer@opalmer.com>
2017-10-28 12:20:28 -0400

    debugging appveyor build

--- .ci/appveyor/test.cmd
+++ .ci/appveyor/test.cmd
@@ -1,4 +1,5 @@
 %WITH_COMPILER% %PYTHON%\Scripts\pip.exe install -r dev_requirements.txt --upgrade --quiet || EXIT 1
-%WITH_COMPILER% %PYTHON%\Scripts\py.test.exe --strict --verbose --cov pywincffi || EXIT 1
+%WITH_COMPILER% %PYTHON%\Scripts\py.test.exe --strict --verbose --cov pywincffi --cov-report=xml || EXIT 1
 %PYTHON%\Scripts\coverage.exe xml || EXIT 1
-%PYTHON%\Scripts\codecov.exe --required || EXIT 1
+dir
+%PYTHON%\Scripts\codecov.exe -X gcov --required || EXIT 1
