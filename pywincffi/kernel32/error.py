4f2b0ba0d3cb026532e5e86db578db050c6ddc5d
Oliver Palmer <oliverpalmer@opalmer.com>
2018-01-13 12:33:05 -0500

    implemented overlapped_error_check()

--- pywincffi/kernel32/error.py
+++ pywincffi/kernel32/error.py
@@ -8,7 +8,8 @@ A module containing functions for dealing with API errors.
 from six import integer_types
 
 from pywincffi.core import dist
-from pywincffi.core.checks import input_check
+from pywincffi.core.checks import NON_ZERO, input_check, error_check
+from pywincffi.exceptions import WindowsAPIError
 
 
 def GetLastError():
@@ -64,3 +65,39 @@ def get_error_message(code=None):
 
     ffi, _ = dist.load()
     return ffi.getwinerror(code)[-1]
+
+
+def overlapped_error_check(function_name, code, lpOverlapped):
+    """
+    This function is a basic error checker for overlapped and non-overlapped
+    cases. At the moment this is mainly intended for internal use and is used
+    by function such as :func:`pywincffi.kernel32.file.ReadFile` and
+    :func:`pywincffi.kernel32.file.WriteFile`.
+
+    The implementation of this function is based on the following
+    documentation by Microsoft:
+
+      * https://support.microsoft.com/en-us/help/156932
+      * https://msdn.microsoft.com/en-us/library/windows/desktop/ms686358
+      * https://msdn.microsoft.com/en-us/library/windows/desktop/aa365683
+
+    :param str function_name:
+        The name of the function being checked.
+
+    :param int code:
+        The return code of the last function call.
+
+    :param lpOverlapped:
+        The overlapped structure being passed into the previously called
+        function.
+    """
+    if lpOverlapped is None:
+        error_check(function_name, code=code, expected=NON_ZERO)
+        return
+
+    # Overlapped IO case.
+    _, library = dist.load()
+    if GetLastError() == library.ERROR_IO_PENDING and code != 0:
+        raise WindowsAPIError(
+            function_name, get_error_message(code), code,
+            return_code=code, expected_return_code=0)
--- tests/test_kernel32/test_error.py
+++ tests/test_kernel32/test_error.py
@@ -1,19 +1,22 @@
 from pywincffi.core import dist
 from pywincffi.dev.testutil import TestCase
+from pywincffi.exceptions import WindowsAPIError
 from pywincffi.kernel32.error import (
-    GetLastError, SetLastError, get_error_message)
+    GetLastError, SetLastError, get_error_message, overlapped_error_check)
 
 
-class TestError(TestCase):
+class Case(TestCase):
     # These test cases will manipulate the current error code. Because
     # this behavior is expected we don't want to run this function.
     def unhandled_error_check(self):
         pass
 
-    def test_get_last_error(self):
+
+class TestGetError(Case):
+    def test_default_get_last_error(self):
         self.assertEqual(GetLastError(), 0)
 
-    def test_get_last_error_after_set(self):
+    def test_value(self):
         try:
             value = long(4242)
         except NameError:
@@ -34,3 +37,23 @@ class TestError(TestCase):
         self.assertEqual(
             get_error_message(library.ERROR_ACCESS_DENIED),
             "Access is denied")
+
+
+class TestOverlappedErrorCheck(Case):
+    def test_non_overlapped_fail(self):
+        with self.assertRaisesRegex(WindowsAPIError, ".+Expected.+0.+"):
+            overlapped_error_check("test", 0, None)
+
+    def test_non_overlapped_pass(self):
+        overlapped_error_check("test", 50, None)
+
+    def test_overlapped_pass(self):
+        _, library = dist.load()
+        SetLastError(library.ERROR_IO_PENDING)
+        overlapped_error_check("test", 0, {})
+
+    def test_overlapped_fail(self):
+        _, library = dist.load()
+        SetLastError(library.ERROR_IO_PENDING)
+        with self.assertRaisesRegex(WindowsAPIError, ".+Expected.+rece.+50.+"):
+            overlapped_error_check("test", 50, {})
